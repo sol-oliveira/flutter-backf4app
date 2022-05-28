e01a4cbf80b5aaf753a44eeac9f4ab7ec4e4ecd4
Sol <sol.oliveira@elevenfinancial.com>
2022-05-28 11:56:56 -0300

    add: variáveis de ambiente

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,17 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding.ensureInitialized();
 
-  const keyApplicationId = '';
-  const keyClientKey = '';
+  final keyApplicationId = dotenv.env['KEY_APPLICATION_ID'];
+  final keyClientKey = dotenv.env['KEY_CLIENT_KEY'];
   const keyParseServerUrl = 'https://parseapi.back4app.com';
 
-  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+  await Parse().initialize(keyApplicationId!, keyParseServerUrl,
       clientKey: keyClientKey, debug: true);
 
   runApp(const MaterialApp(
